ccf21ac748ba33f226e1c6c0160fcf4652af9a5d
Nifaz <64573671+nifazzz10@users.noreply.github.com>
2023-03-10 15:03:46 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,11 @@
+const data={
+    id:1,
+    title:"Daily Fuel Price Webapp",
+    desc:"This project was overseen by our college's Mobile App Laboratory and uses web scraping to show the data on a site where the API is not obtained from any publicly accessible website and displayed using react",
+    desc:"This project was overseen by our college's Mobile App Laboratory and uses web scraping to show the data on a site where the API is not obtained from any publicly accessible website and displayed using react",
+    desc:"This project was overseen by our college's Mobile App Laboratory and uses web scraping to show the data on a site where the API is not obtained from any publicly accessible website and displayed using react",
+   
+
+  }
+
+export default data;
